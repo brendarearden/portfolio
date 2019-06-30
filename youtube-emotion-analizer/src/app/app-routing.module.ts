6741f29715590c942bc21c031cde8fd0d3c0412c
Brenda Rearden <brearden@asu.edu>
2019-06-30 00:06:05 -0700

    added routes import in routing module. added route tag to app html

--- youtube-emotion-analizer/src/app/app-routing.module.ts
+++ youtube-emotion-analizer/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
