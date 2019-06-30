a77065209476dd9c7fc3853e01f818aeb5e8cd5f
Brenda Rearden <brearden@asu.edu>
2019-06-30 00:07:29 -0700

    initial code for navigation bar / header

--- youtube-emotion-analizer/src/app/app.module.ts
+++ youtube-emotion-analizer/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavHeaderComponent
   ],
   imports: [
     BrowserModule,
