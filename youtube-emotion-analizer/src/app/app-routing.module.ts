647120e4269413896ac9d15d71ac5e0e9fb11c79
Brenda Rearden <brearden@asu.edu>
2019-06-29 23:56:48 -0700

    added initial home component code and added to routing

--- youtube-emotion-analizer/src/app/app-routing.module.ts
+++ youtube-emotion-analizer/src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [],
