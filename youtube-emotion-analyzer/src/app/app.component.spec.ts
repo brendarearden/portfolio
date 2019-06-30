86de1b73390c830679ecd9914ef499f0f9a00f8d
Brenda Rearden <brearden@asu.edu>
2019-06-30 00:30:37 -0700

    correcting final misspelling issues

--- youtube-emotion-analyzer/src/app/app.component.spec.ts
+++ youtube-emotion-analyzer/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'youtube-emotion-analizer'`, () => {
+  it(`should have as title 'youtube-emotion-analyzer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('youtube-emotion-analizer');
+    expect(app.title).toEqual('youtube-emotion-analyzer');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to youtube-emotion-analizer!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to youtube-emotion-analyzer!');
   });
 });
