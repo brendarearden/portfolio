5e4b40966a602b38750ee07cf144deb2bf427a2c
Brenda Rearden <brearden@asu.edu>
2019-07-08 22:23:35 -0700

    added error handling for http requests. added base call for getChannels

--- youtube-emotion-analyzer/src/app/app.module.ts
+++ youtube-emotion-analyzer/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -16,6 +16,7 @@ import { SearchComponent } from './search/search.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- youtube-emotion-analyzer/src/app/search/search.service.ts
+++ youtube-emotion-analyzer/src/app/search/search.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  url =
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
+
+  getChannel(){
+    return this.http.get(this.url, {headers?: })
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+
+
 }
